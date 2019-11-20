b95d531b14c8ed98ee76e8ce55ed868be65f7c6a
rafaelreisramos <rrramos@gmail.com>
2019-11-20 09:47:33 -0300

    add user avatar return

--- backend/src/app/controllers/ScheduleController.js
+++ backend/src/app/controllers/ScheduleController.js
@@ -24,6 +24,13 @@ class ScheduleController {
           [Op.between]: [startOfDay(date), endOfDay(date)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
     });
 
     return res.json(appointments);
