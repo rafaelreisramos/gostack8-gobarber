71e6b98152cd2241ce77c082b4877b5eb240df14
rafaelreisramos <rrramos@gmail.com>
2019-11-20 18:39:37 -0300

    toast error messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
 
 import { Provider } from 'react-redux';
 
@@ -20,6 +21,7 @@ function App() {
         <Router history={history}>
           <Routes />
           <GlobalStyle />
+          <ToastContainer autoClose={3000} />
         </Router>
       </PersistGate>
     </Provider>
