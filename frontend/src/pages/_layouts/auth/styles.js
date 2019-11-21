5c96a43100c3c9bf1cfce45e56598a01a0f37136
rafaelreisramos <rrramos@gmail.com>
2019-11-21 08:33:41 -0300

    fix auth css styles

--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -40,32 +40,32 @@ export const Content = styled.div`
       margin: 0 0 10px;
       font-weight: bold;
     }
-  }
 
-  button {
-    margin: 5px 0 0;
-    height: 44px;
-    background: #3b9eff;
-    font-weight: bold;
-    color: #fff;
-    border: 0;
-    border-radius: 4px;
-    font-size: 16px;
-    transition: background 0.2s;
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #3b9eff;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
 
-    &:hover {
-      background: ${darken(0.03, '#3b9eff')};
+      &:hover {
+        background: ${darken(0.03, '#3b9eff')};
+      }
     }
-  }
 
-  a {
-    color: #fff;
-    margin-top: 15px;
-    font-size: 16px;
-    opacity: 0.8;
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
 
-    &:hover {
-      opacity: 1;
+      &:hover {
+        opacity: 1;
+      }
     }
   }
 `;
