c0697e6bd47e6487a0a534adab235a66823b1476
rafaelreisramos <rrramos@gmail.com>
2019-11-21 09:56:34 -0300

    add/update profile picture

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
     'no-underscore-dangle': 'off',
+    camelcase: 'off',
     'no-console': ['error', { allow: ['tron'] }],
   },
   settings: {
