31fd3c58c9c35db486afd932b0ec689a136cdf80
rafaelreisramos <rrramos@gmail.com>
2019-12-05 08:47:19 -0300

    background linear gradient component

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Background from '~/components/background';
+
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <Text>SignUp</Text>;
+  return (
+    <Background>
+      <Text>SignUp</Text>
+    </Background>
+  );
 }
